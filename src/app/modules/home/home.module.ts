29a42070c7d5d92b2617235ff668522bcc9ad3a5
Michael Stape <mstape2000@gmail.com>
2022-07-12 11:05:35 -0400

    new components

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatIconModule
   ],
   exports: [
     HomeComponent
   ]
 })
-export class HomeModule {
-
-}
+export class HomeModule { }
